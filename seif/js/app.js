3bbdb708cee56cdda69939590d9f249a7989a1ee
seif amr mohammed <68325899+Seif-Elnasr-FCI@users.noreply.github.com>
2020-09-25 00:35:51 +0200

    Add files via upload

---
+++ seif/js/app.js
@@ -0,0 +1,26 @@
+
+const link1 = document.getElementById('link1')
+
+
+link1.onclick = function() {
+
+    link1.setAttribute('href' , '#section1')
+}
+
+
+const link2 = document.getElementById('link2')
+
+
+link2.onclick = function() {
+
+    link2.setAttribute('href' , '#section2')
+}
+
+const link3 = document.getElementById('link3')
+
+
+link3.onclick = function() {
+
+ link3.setAttribute('href' , '#section3')
+}
+
